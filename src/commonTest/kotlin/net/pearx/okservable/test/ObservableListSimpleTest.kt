f54ddea15d6b08ec14f40a0d2ac6677e33b56937
mrAppleXZ <mr.applexz@gmail.com>
2020-02-17 18:04:32 +0500

    Work on ObservableListSimple test

--- src/commonTest/kotlin/net/pearx/okservable/test/ObservableListSimpleTest.kt
+++ src/commonTest/kotlin/net/pearx/okservable/test/ObservableListSimpleTest.kt
@@ -7,9 +7,11 @@
 
 package net.pearx.okservable.test
 
+import net.pearx.okservable.collection.ObservableListSimple
 import net.pearx.okservable.collection.observableSimple
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 class ObservableListSimpleTest {
     inner class TestingContext(empty: Boolean = false) {
@@ -74,6 +76,27 @@ class ObservableListSimpleTest {
     }
 
     @Test
+    fun testAddWithIndex() {
+        with(TestingContext()) {
+            collection.add(2, "theevilroot1")
+            assertEquals(true, modified)
+            assertEquals(listOf<String?>("theevilroot", "root", "theevilroot1", null, ""), collection.base)
+        }
+
+        with(TestingContext()) {
+            collection.add(0, "theevilroot")
+            assertEquals(true, modified)
+            assertEquals(listOf<String?>("theevilroot", "theevilroot", "root", null, ""), collection.base)
+        }
+
+        with(TestingContext()) {
+            assertFailsWith<IndexOutOfBoundsException> { collection.add(5, "theevilroot") }
+            assertEquals(false, modified)
+            assertEquals(listOf<String?>("theevilroot", "root", null, ""), collection.base)
+        }
+    }
+
+    @Test
     fun testAddAll() {
         with(TestingContext()) {
             collection.addAll(listOf(null, "root"))
@@ -95,6 +118,33 @@ class ObservableListSimpleTest {
 
         with(TestingContext()) {
             collection.addAll(listOf())
+            assertEquals(false, modified)
+            assertEquals(listOf<String?>("theevilroot", "root", null, ""), collection.base)
+        }
+    }
+
+    @Test
+    fun testAddAllWithIndex() {
+        with(TestingContext()) {
+            collection.addAll(0, listOf(null, "root"))
+            assertEquals(true, modified)
+            assertEquals(listOf<String?>(null, "root", "theevilroot", "root", null, ""), collection.base)
+        }
+
+        with(TestingContext()) {
+            collection.addAll(4, listOf("openwrt", "ddwrt"))
+            assertEquals(true, modified)
+            assertEquals(listOf<String?>("theevilroot", "root", null, "", "openwrt", "ddwrt"), collection.base)
+        }
+
+        with(TestingContext()) {
+            collection.addAll(2, listOf())
+            assertEquals(false, modified)
+            assertEquals(listOf<String?>("theevilroot", "root", null, ""), collection.base)
+        }
+
+        with(TestingContext()) {
+            assertFailsWith<IndexOutOfBoundsException> { collection.addAll(5, listOf("theevilroot")) }
             assertEquals(false, modified)
             assertEquals(listOf<String?>("theevilroot", "root", null, ""), collection.base)
         }
@@ -143,6 +193,94 @@ class ObservableListSimpleTest {
     }
 
     @Test
+    fun testListIterator() {
+        with(TestingContext()) {
+            val iter = collection.listIterator()
+            val indices = mutableListOf<Int>()
+            val elements = mutableListOf<String?>()
+            var num = 0
+            while(iter.hasNext()) {
+                indices += iter.nextIndex()
+                elements += iter.next()
+                num++
+            }
+            assertEquals(4, num)
+            assertEquals(false, modified)
+            assertEquals(listOf<String?>("theevilroot", "root", null, ""), elements)
+            assertEquals((0..3).toList(), indices)
+            assertEquals(listOf<String?>("theevilroot", "root", null, ""), collection.base)
+        }
+
+        with(TestingContext()) {
+            val iter = collection.listIterator()
+            val indices = mutableListOf<Int>()
+            val elements = mutableListOf<String?>()
+            var num = 0
+            while(iter.hasNext()) {
+                indices += iter.nextIndex()
+                val el = iter.next()
+                if(el == "theevilroot")
+                    iter.add("five")
+                if(el == "")
+                    iter.set("cake")
+                if(el == null)
+                    iter.remove()
+                elements += el
+                num++
+            }
+            assertEquals(4, num)
+            assertEquals(true, modified)
+            assertEquals(listOf<String?>("theevilroot", "root", null, ""), elements)
+            assertEquals(listOf(0, 2, 3, 3), indices)
+            assertEquals(listOf<String?>("theevilroot", "five", "root", "cake"), collection.base)
+        }
+    }
+
+    @Test
+    fun testListIteratorFromIndex() {
+        with(TestingContext()) {
+            val iter = collection.listIterator(4)
+            val indices = mutableListOf<Int>()
+            val elements = mutableListOf<String?>()
+            var num = 0
+            while(iter.hasPrevious()) {
+                indices += iter.previousIndex()
+                elements += iter.previous()
+                num++
+            }
+            assertEquals(4, num)
+            assertEquals(false, modified)
+            assertEquals(listOf<String?>("", null, "root", "theevilroot"), elements)
+            assertEquals((3 downTo 0).toList(), indices)
+            assertEquals(listOf<String?>("theevilroot", "root", null, ""), collection.base)
+        }
+
+        with(TestingContext()) {
+            val iter = collection.listIterator(4)
+            val indices = mutableListOf<Int>()
+            val elements = mutableListOf<String?>()
+            var num = 0
+            while(iter.hasPrevious()) {
+                indices += iter.previousIndex()
+                val el = iter.previous()
+                if(el == "theevilroot")
+                    iter.add("five")
+                if(el == "")
+                    iter.set("cake")
+                if(el == null)
+                    iter.remove()
+                elements += el
+                num++
+            }
+            assertEquals(5, num)
+            assertEquals(true, modified)
+            assertEquals(listOf<String?>("", null, "root", "theevilroot", "five"), elements)
+            assertEquals(listOf(3, 2, 1, 0, 0), indices)
+            assertEquals(listOf<String?>("five", "theevilroot", "root", "cake"), collection.base)
+        }
+    }
+
+    @Test
     fun testRemove() {
         with(TestingContext()) {
             collection.remove("theevilroot1")
@@ -154,6 +292,36 @@ class ObservableListSimpleTest {
             collection.remove("theevilroot")
             assertEquals(true, modified)
             assertEquals(listOf<String?>("root", null, ""), collection.base)
+        }
+    }
+
+    @Test
+    fun testRemoveAt() {
+        with(TestingContext()) {
+            collection.removeAt(0)
+            assertEquals(true, modified)
+            assertEquals(listOf<String?>("root", null, ""), collection.base)
+        }
+
+        with(TestingContext()) {
+            assertFailsWith<IndexOutOfBoundsException> { collection.removeAt(4) }
+            assertEquals(false, modified)
+            assertEquals(listOf<String?>("theevilroot", "root", null, ""), collection.base)
+        }
+    }
+
+    @Test
+    fun testSet() {
+        with(TestingContext()) {
+            collection[0] = "tea"
+            assertEquals(true, modified)
+            assertEquals(listOf<String?>("tea", "root", null, ""), collection.base)
+        }
+
+        with(TestingContext()) {
+            assertFailsWith<IndexOutOfBoundsException> { collection[4] = "povar" }
+            assertEquals(false, modified)
+            assertEquals(listOf<String?>("theevilroot", "root", null, ""), collection.base)
         }
     }
 
